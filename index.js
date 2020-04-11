bac0c0ee72bcbea67454fd14b3241fedee0bb1f2
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-12 09:14:47 +1000

    added get /send

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
+
+app.get('/send', (req, res) => {
+  res.send('HEY send!')
+})
 app.listen(3000, () => console.log('Server running on port 3000'))
