f297a2fa3476a3dfa6232dac79e2a084c9a39ee5
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-12 09:59:37 +1000

    test cors and 'require'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const router = express.Router();
 const nodemailer = require('nodemailer');
-// const cors = require('cors');
+const cors = require('cors');
 // let bodyParser = require('body-parser');
 // const {user, password} = require('/config.js');
 // app.use(cors())
