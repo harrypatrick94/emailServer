f5ce51aa69000fcd3264d8f270371cf4759b15f6
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-12 09:56:51 +1000

    test router

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const router = express.Router();
+const router = express.Router();
 // const nodemailer = require('nodemailer');
 // const cors = require('cors');
 // let bodyParser = require('body-parser');
