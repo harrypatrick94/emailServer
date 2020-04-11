a54da75e15e053445e0669e847895f416089ccce
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-12 09:54:17 +1000

    test config

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express()
 // const nodemailer = require('nodemailer');
 // const cors = require('cors');
 // let bodyParser = require('body-parser');
-// const {user, password} = require('/config.js');
-app.use(cors())
+const {user, password} = require('/config.js');
+// app.use(cors())
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
 
 app.get('/send', (req, res) => {
-  res.send(`hey send`)
+  res.send(`hey ${user}`)
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
