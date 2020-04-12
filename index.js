e0880505aa3a6e6b743675e16cfdbbd4784f1649
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-12 10:01:37 +1000

    test body-parser and 'require'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-// let bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 // const {user, password} = require('/config.js');
 // app.use(cors())
 app.get('/', (req, res) => {
