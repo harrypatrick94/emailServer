39796a7e0590e5a0543b3f819baa10815a00bf56
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-12 10:13:46 +1000

    test send email post and functionality

--- index.js
+++ index.js
@@ -37,4 +37,30 @@ transporter.verify((error, success) => {
   }
 });
 
+router.post('/send', (req, res, next) => {
+  let name = req.body.name
+  let email = req.body.email
+  let message = req.body.message
+  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  let mail = {
+    from: name,
+    to: 'harry.g.patrick94@gmail.com',  // Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
